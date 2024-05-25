548a380552b562c806a48e964b5481862808a84c
Monet <monetpaul23@gmail.com>
2024-05-25 19:55:05 -0400

    setup for  design of effect

--- src/Components/BackgroundEfffect.jsx
+++ src/Components/BackgroundEfffect.jsx
@@ -7,7 +7,7 @@ import  testIdea  from "../assets/test.json"
 const BackgroundEfffect = () => {
   return (
     <>
-    <div >  
+    <div className='bg-zinc-800 overflow-hidden' >  
 <Lottie className='max-w-56 max-h-56 rotate-[-30deg]'  animationData={testIdea}  />
 </div>
     </>
